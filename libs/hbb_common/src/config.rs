eb7ab6e78950c28b82ec37fc0541aadd6141cf86
invoker-kael <63380399+invoker-kael@users.noreply.github.com>
2024-09-29 21:38:01 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.wocao.cyou:8443"];
+pub const PUBLIC_RS_PUB_KEY: &str = "7R5ti8OrBf7GLwRaG8XVrTemZIBOtAp8Aks2y4+WazE=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
